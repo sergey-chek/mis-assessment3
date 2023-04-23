655862aac6b14b09138e9ddffaf694da28b07da7
Sergey Chekurin <schek.it@gmail.com>
2023-04-24 03:04:52 +1000

    business logic added

--- assessment3.py
+++ assessment3.py
@@ -12,16 +12,38 @@ class User:
         self.previous_passwords = []
         self.orders = []
 
+class MenuItem:
+    def __init__(self, id, name, price):
+        self.id = id
+        self.name = name
+        self.price = price
+
+    def item_text(self):
+        return f'Enter {self.id} for {self.name}\t\t\tPrice AUD {self.price}'
 
 class Order:
     def __init__(self):
+        self.id = None
+        self.date = None
         self.ordering_mode = None
+        self.items = []
 
     def set_ordering_mode(self, ordering_mode):
         self.ordering_mode = ordering_mode
 
     def reset(self):
         self.ordering_mode = None
+        self.items = []
+        self.total_price = 0
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def get_total_price(self):
+        total_price = 0
+        for item in self.items:
+            total_price += item.price
+        return total_price
 
 
 class Account:
@@ -104,7 +126,7 @@ class Account:
     def get_signed_in_user(self):
         return self.signed_in_user
 
-    def exit_account(self):
+    def log_out(self):
         self.signed_in_user = None
 
 
@@ -154,17 +176,16 @@ class StartMenu(Menu):
         self.allowed_input = ['1', '2', '3']
 
     def _user_choice_processing(self, user_choice):
+        current_order.reset()
+        account.log_out()
         if user_choice == self.allowed_input[0]:     # 1 Sign Up
             account.sign_up()
         elif user_choice == self.allowed_input[1]:   # 2 Sign In
             if account.sign_in():
-                sign_in_menu = SignInMenu()
-                sign_in_menu.show_menu()
-                user_choice = sign_in_menu.get_user_choice()
-                sign_in_menu.user_choice_processing(user_choice)
+                SignInMenu().execute()
         elif user_choice == self.allowed_input[2]:   # 3 Quit
             print('\nThank you for using the Application!')
-            return 'QUIT'
+            self.action = 'QUIT'
 
 
 class SignInMenu(Menu):
@@ -176,21 +197,19 @@ class SignInMenu(Menu):
                          'Please Enter 2.3 for Logout.\n'
         self.allowed_input = ['2.1', '2.2', '2.3']
 
-    def user_choice_processing(self, user_choice):
+    def _user_choice_processing(self, user_choice):
         if user_choice == self.allowed_input[0]:    # 2.1 Start Ordering
             self._start_ordering()
         elif user_choice == self.allowed_input[1]:  # 2.2 Print Statistics
             self._print_statistics()
         elif user_choice == self.allowed_input[2]:  # 2.3 Logout
-            account.exit_account()
+            account.log_out()
             current_order.reset()
             print('You have successfully Logged out!')
 
     def _start_ordering(self):
         ordering_menu = OrderingMenu()
-        ordering_menu.show_menu()
-        user_choice = ordering_menu.get_user_choice()
-        ordering_menu.user_choice_processing(user_choice)
+        ordering_menu.execute()
         if ordering_menu.action == 'TO_ORDERING_MENU':
             self._start_ordering()
 
@@ -207,20 +226,19 @@ class OrderingMenu(Menu):
                          'Please Enter 3 to go to Login Page.\n'
         self.allowed_input = ['1', '2', '3']
 
-    def user_choice_processing(self, user_choice):
+    def _user_choice_processing(self, user_choice):
         if user_choice == self.allowed_input[0]:    # 1 Dine In
-            current_order.set_ordering_mode('DINE_IN')
+            FoodMenu(show_drinks=True).execute()
         elif user_choice == self.allowed_input[1]:  # 2 Order Online
             self._order_online()
+            FoodMenu(show_drinks=False).execute()
         elif user_choice == self.allowed_input[2]:  # 3 Login Page
-            account.exit_account()
+            account.log_out()
             current_order.reset()
 
     def _order_online(self):
         ordering_online_menu = OrderingOnlineMenu()
-        ordering_online_menu.show_menu()
-        user_choice = ordering_online_menu.get_user_choice()
-        ordering_online_menu.user_choice_processing(user_choice)
+        ordering_online_menu.execute()
         if ordering_online_menu.action == 'TO_ORDERING_MENU':
             self.action = ordering_online_menu.action
 
@@ -234,7 +252,7 @@ class OrderingOnlineMenu(Menu):
                          'Enter 3 to go to Previous Menu.\n'
         self.allowed_input = ['1', '2', '3']
 
-    def user_choice_processing(self, user_choice):
+    def _user_choice_processing(self, user_choice):
         if user_choice == self.allowed_input[0]:  # 1 Self Pickup
             current_order.set_ordering_mode('SELF_PICKUP')
         elif user_choice == self.allowed_input[1]:  # 2 Home Delivery
@@ -243,10 +261,80 @@ class OrderingOnlineMenu(Menu):
             self.action = 'TO_ORDERING_MENU'
 
 
+class FoodMenu(Menu):
+    def __init__(self, show_drinks):
+        super().__init__()
+        self.show_drinks = show_drinks
+        self.items = [
+            MenuItem(1, 'Noodles', 2),
+            MenuItem(2, 'Sandwich', 4),
+            MenuItem(3, 'Dumpling', 6),
+            MenuItem(4, 'Muffins', 8),
+            MenuItem(5, 'Pasta', 10),
+            MenuItem(6, 'Pizza', 20)
+        ]
+        if show_drinks:
+            self.menu_text = 'Enter 1 for Noodles   Price AUD 2\n' \
+                             'Enter 2 for Sandwich  Price AUD 4\n' \
+                             'Enter 3 for Dumpling  Price AUD 6\n' \
+                             'Enter 4 for Muffins   Price AUD 8\n' \
+                             'Enter 5 for Pasta     Price AUD 10\n' \
+                             'Enter 6 for Pizza     Price AUD 20\n' \
+                             'Enter 7 for drinks menu'
+        else:
+            self.menu_text = 'Enter 1 for Noodles   Price AUD 2\n' \
+                             'Enter 2 for Sandwich  Price AUD 4\n' \
+                             'Enter 3 for Dumpling  Price AUD 6\n' \
+                             'Enter 4 for Muffins   Price AUD 8\n' \
+                             'Enter 5 for Pasta     Price AUD 10\n' \
+                             'Enter 6 for Pizza     Price AUD 20\n' \
+                             'Enter 7 for Checkout'
+        self.allowed_input = ['1', '2', '3', '4', '5', '6', '7']
+
+    def _user_choice_processing(self, user_choice):
+        if user_choice == '7' and self.show_drinks:
+            DrinksMenu().execute()
+        elif user_choice == '7' and not self.show_drinks:
+            pass  # todo to checkout
+        else:
+            for item in self.items:
+                if item.id == int(user_choice):
+                    current_order.add_item(item)
+                    print(f'\n{item.name} added to your order. Total price = {current_order.get_total_price()} AUD\n')
+                    break
+            FoodMenu(show_drinks=self.show_drinks).execute()
+
+
+class DrinksMenu(Menu):
+    def __init__(self):
+        super().__init__()
+        self.items = [
+            MenuItem(1, 'Coffee', 2),
+            MenuItem(2, 'Colddrink', 4),
+            MenuItem(3, 'Shake', 6),
+        ]
+        self.menu_text = 'Enter 1 for Coffee        Price AUD 2\n' \
+                         'Enter 2 for Colddrink     Price AUD 4\n' \
+                         'Enter 3 for Shake         Price AUD 6\n' \
+                         'Enter 4 for Checkout'
+        self.allowed_input = ['1', '2', '3', '4']
+
+    def _user_choice_processing(self, user_choice):
+        if user_choice == '4':
+            self.action = 'TO_CHECKOUT'
+            pass # todo
+        else:
+            for item in self.items:
+                if item.id == int(user_choice):
+                    current_order.add_item(item)
+                    print(f'\n{item.name} added to your order. Total price = {current_order.get_total_price()} AUD\n')
+                    break
+            DrinksMenu().execute()
+
 # The main program begins
 users = []
 
-user = User('Sergey', 'Brisben', '0987654321', 'S@2', '01/11/1987')
+user = User('Sergey', 'Brisbane', '0987654321', 'S@2', '01/11/1987')
 users.append(user)
 
 account = Account()
@@ -254,7 +342,6 @@ current_order = Order()
 
 while True:
     start_menu = StartMenu()
-    start_menu.show_menu()
-    user_choice = start_menu.get_user_choice()
-    if start_menu.user_choice_processing(user_choice) == 'QUIT':
+    start_menu.execute()
+    if start_menu.action == 'QUIT':
         break
